7017615cd8a6e594c355ede22e7395149505120f
Shutu <996822362@qq.com>
2019-05-25 12:19:47 +0800

    php

--- application/api/controller/Index.php
+++ application/api/controller/Index.php
@@ -16,18 +16,62 @@ class Index extends Base
 
     public function getMenu()
     {
-        //获取请求头
-        $token = Request::instance()->header('Authorization');
-        $result = Db::table('user')->where('token', $token)->find();
-        if (empty($result)) {
-            return json(['code' => 10002, 'msg' => '请重新登录']);
-        }
+        $this->checkHeader();
         $roleId = \session('roleId');
         $menuList = $this->getMenuList($roleId);
         if (!empty($menuList)) {
             return json(['code' => 10000, 'msg' => '获取成功', 'data' => $menuList]);
         } else {
             return json(['code' => 10001, 'msg' => '数据异常']);
+        }
+    }
+
+    public  function getRole(){
+        $this->checkHeader();
+        //当前页码
+        $currentPage = input('post.currentPage');
+        $countList = Db::table('role')->field('count(*)')->find();
+        $count =  $countList['count(*)'];
+        $pageSize = input('post.PageSize');
+        $startNum = ($currentPage - 1)*$pageSize;
+        $limit = "$startNum,$pageSize";
+        $roleList = Db::table('role')->limit($limit)->select();
+        if(!empty($roleList)){
+            return json(['code'=>10000,'msg'=>'查询成功','data'=>$roleList,'count'=>$count]);
+        }else{
+            return json(['code'=>10001,'msg'=>'查询失败']);
+        }
+    }
+
+    public function addRole(){
+        $this->checkHeader();
+        $name = input('post.name');
+        $desc = input('post.desc');
+        $data = ['title'=>$name,'role_desc'=>$desc];
+        $result = Db::table('role')->insertGetId($data);
+        if(!empty($result)){
+            return json(['code'=>10000,'msg'=>'添加成功']);
+        }else{
+            return json(['code'=>10001,'msg'=>'添加失败']);
+        }
+    }
+
+    public function delRole(){
+        $roleId = input('post.id');
+        $result = Db::table('role')->delete($roleId);
+        if(!empty($result)){
+            return json(['code'=>10000,'msg'=>'删除成功']);
+        }else{
+            return json(['code'=>10001,'msg'=>'删除失败']);
+        }
+    }
+
+    public function checkHeader(){
+        //获取请求头
+        $token = Request::instance()->header('Authorization');
+        $result = Db::table('user')->where('token', $token)->find();
+        if (empty($result)) {
+            return json(['code' => 10002, 'msg' => '请重新登录']);
         }
     }
 
